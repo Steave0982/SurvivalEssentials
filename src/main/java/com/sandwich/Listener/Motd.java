1cce6240a5d26dac4a773bf8b11ee8164933e7c5
Alexander Milanovich <steave0982@outlook.com>
2019-07-24 12:20:12 -0400

    Simple MOTD

---
+++ src/main/java/com/sandwich/Listener/Motd.java
@@ -0,0 +1,30 @@
+package com.sandwich.Listener;
+
+import com.sandwich.SurvivalEssentials;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.ServerListPingEvent;
+
+public class Motd implements Listener {
+
+    private SurvivalEssentials se;
+
+
+    public Motd(SurvivalEssentials se) {
+        this.se = se;
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onServerPing(ServerListPingEvent event) {
+        String motd1 = se.getConfig().getString("MOTD.Line-1");
+        String motd2 = se.getConfig().getString("MOTD.Line-2");
+        motd1 = motd1.replace("&", "§");
+        motd2 = motd2.replace("&", "§");
+        event.setMotd(String.valueOf(motd1) + "\n" + motd2);
+        if (se.getConfig().get("Server-Maximum-Players.Modify") != null &&
+                se.getConfig().getBoolean("Server-Maximum-Players.Modify")) {
+            event.setMaxPlayers(se.getConfig().getInt("Server-Maximum-Players.Maximum-Players"));
+        }
+    }
+}
